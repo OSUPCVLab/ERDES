783576649470cbb9ef7c29fedfae255c028b9a6f
yaseminozkut <ozkutyasemin@gmail.com>
2025-07-09 16:56:21 -0400

    Create bash files for running all the experiments with tmux

---
+++ run_all_normal_vs_rd_exp.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+SESSION="normal_vs_rd_experiment"
+
+tmux new-session -d -s $SESSION
+tmux send-keys -t $SESSION "conda activate erdes" C-m
+tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.devices=3 experiment=normal_vs_rd/efficientnet |& tee tmux_outputs/normal_vs_rd/efficientnet_training.log" C-m
+tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.devices=3 experiment=normal_vs_rd/resnet3d |& tee tmux_outputs/normal_vs_rd/resnet3d_training.log" C-m
+tmux send-keys -t $SESSION "python3 src/train.py trainer=gpu experiment=normal_vs_rd/swinunetr |& tee tmux_outputs/normal_vs_rd/swinunetr_training.log" C-m
+tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.devices=3 experiment=normal_vs_rd/unet3d |& tee tmux_outputs/normal_vs_rd/unet3d_training.log" C-m
+tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.devices=3 experiment=normal_vs_rd/unetplusplus |& tee tmux_outputs/normal_vs_rd/unetplusplus_training.log" C-m
+tmux send-keys -t $SESSION "python3 src/train.py trainer=gpu experiment=normal_vs_rd/unetr |& tee tmux_outputs/normal_vs_rd/unetr_training.log" C-m
+tmux send-keys -t $SESSION "python3 src/train.py trainer=gpu experiment=normal_vs_rd/vit |& tee tmux_outputs/normal_vs_rd/vit_training.log" C-m
+tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.devices=3 experiment=normal_vs_rd/vnet |& tee tmux_outputs/normal_vs_rd/vnet_training.log" C-m
+tmux attach -t $SESSION
