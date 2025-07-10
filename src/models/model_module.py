7496eba57d00f93dc70a9e4b8cc8784d27446e3d
yaseminozkut <ozkutyasemin@gmail.com>
2025-07-10 04:51:14 -0400

    Add F1 score and rename recall as sensitivity

--- src/models/model_module.py
+++ src/models/model_module.py
@@ -4,7 +4,7 @@ import torch
 from lightning import LightningModule
 from torchmetrics import MaxMetric, MeanMetric
 from torchmetrics.classification.accuracy import Accuracy
-from torchmetrics.classification import Precision, Recall, Specificity
+from torchmetrics.classification import Precision, Recall, Specificity, F1Score
 
 class ModelModule(LightningModule):
     """Example of a `LightningModule` for MNIST classification.
@@ -78,16 +78,19 @@ class ModelModule(LightningModule):
 
         # for precision, recall (sensitivity), specificity metric
         self.train_precision = Precision(task="binary", threshold=0.5)
-        self.train_recall = Recall(task="binary", threshold=0.5)
+        self.train_sensitivity = Recall(task="binary", threshold=0.5)
         self.train_specificity = Specificity(task="binary", threshold=0.5)
+        self.train_f1 = F1Score(task="binary", threshold=0.5)
 
         self.val_precision = Precision(task="binary", threshold=0.5)
-        self.val_recall = Recall(task="binary", threshold=0.5)
+        self.val_sensitivity = Recall(task="binary", threshold=0.5)
         self.val_specificity = Specificity(task="binary", threshold=0.5)
+        self.val_f1   = F1Score(task="binary", threshold=0.5)
 
         self.test_precision = Precision(task="binary", threshold=0.5)
-        self.test_recall = Recall(task="binary", threshold=0.5)
+        self.test_sensitivity = Recall(task="binary", threshold=0.5)
         self.test_specificity = Specificity(task="binary", threshold=0.5)
+        self.test_f1  = F1Score(task="binary", threshold=0.5)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Perform a forward pass through the model `self.net`.
@@ -141,13 +144,15 @@ class ModelModule(LightningModule):
         self.train_loss(loss)
         self.train_acc(preds, targets)
         self.train_precision(preds, targets)
-        self.train_recall(preds, targets)
+        self.train_sensitivity(preds, targets)
         self.train_specificity(preds, targets)
+        self.train_f1(preds, targets)
         self.log("train/loss", self.train_loss, on_step=False, on_epoch=True, prog_bar=True)
         self.log("train/acc", self.train_acc, on_step=False, on_epoch=True, prog_bar=True)
         self.log("train/precision", self.train_precision, on_step=False, on_epoch=True, prog_bar=True)
-        self.log("train/recall", self.train_recall, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("train/sensitivity", self.train_sensitivity, on_step=False, on_epoch=True, prog_bar=True)
         self.log("train/specificity", self.train_specificity, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("train/f1", self.train_f1, on_step=False, on_epoch=True, prog_bar=True)
 
         # return loss or backpropagation will fail
         return loss
@@ -159,8 +164,9 @@ class ModelModule(LightningModule):
             f"Train Loss: {self.train_loss.compute():.3f}, "
             f"Train Acc: {self.train_acc.compute():.3f}, "
             f"Train Precision: {self.train_precision.compute():.3f}, "
-            f"Train Recall: {self.train_recall.compute():.3f}, "
+            f"Train Sensitivity: {self.train_sensitivity.compute():.3f}, "
             f"Train Specificity: {self.train_specificity.compute():.3f}, "
+            f"Train F1: {self.train_f1.compute():.3f}, "
         )
 
     def validation_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> None:
@@ -176,13 +182,15 @@ class ModelModule(LightningModule):
         self.val_loss(loss)
         self.val_acc(preds, targets)
         self.val_precision(preds, targets)
-        self.val_recall(preds, targets)
+        self.val_sensitivity(preds, targets)
         self.val_specificity(preds, targets)
+        self.val_f1(preds, targets)
         self.log("val/loss", self.val_loss, on_step=False, on_epoch=True, prog_bar=True)
         self.log("val/acc", self.val_acc, on_step=False, on_epoch=True, prog_bar=True)
         self.log("val/precision", self.val_precision, on_step=False, on_epoch=True, prog_bar=True)
-        self.log("val/recall", self.val_recall, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("val/sensitivity", self.val_sensitivity, on_step=False, on_epoch=True, prog_bar=True)
         self.log("val/specificity", self.val_specificity, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("val/f1", self.val_f1, on_step=False, on_epoch=True, prog_bar=True)
 
     def on_validation_epoch_end(self) -> None:
         "Lightning hook that is called when a validation epoch ends."
@@ -196,8 +204,9 @@ class ModelModule(LightningModule):
             f"Val Loss: {self.val_loss.compute():.3f}, "
             f"Val Acc: {self.val_acc.compute():.3f}, "
             f"Val Precision: {self.val_precision.compute():.3f}, "
-            f"Val Recall: {self.val_recall.compute():.3f}, "
+            f"Val Sensitivity: {self.val_sensitivity.compute():.3f}, "
             f"Val Specificity: {self.val_specificity.compute():.3f}, "
+            f"Val F1: {self.val_f1.compute():.3f}, "
         )
 
     def test_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> None:
@@ -213,13 +222,15 @@ class ModelModule(LightningModule):
         self.test_loss(loss)
         self.test_acc(preds, targets)
         self.test_precision(preds, targets)
-        self.test_recall(preds, targets)
+        self.test_sensitivity(preds, targets)
         self.test_specificity(preds, targets)
+        self.test_f1(preds, targets)
         self.log("test/loss", self.test_loss, on_step=False, on_epoch=True, prog_bar=True)
         self.log("test/acc", self.test_acc, on_step=False, on_epoch=True, prog_bar=True)
         self.log("test/precision", self.test_precision, on_step=False, on_epoch=True, prog_bar=True)
-        self.log("test/recall", self.test_recall, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("test/sensitivity", self.test_sensitivity, on_step=False, on_epoch=True, prog_bar=True)
         self.log("test/specificity", self.test_specificity, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("test/f1", self.test_f1, on_step=False, on_epoch=True, prog_bar=True)
 
     def on_test_epoch_end(self) -> None:
         """Lightning hook that is called when a test epoch ends."""
@@ -228,8 +239,9 @@ class ModelModule(LightningModule):
             f"Test Loss: {self.test_loss.compute():.3f}, "
             f"Test Acc: {self.test_acc.compute():.3f}, "
             f"Test Precision: {self.test_precision.compute():.3f}, "
-            f"Test Recall: {self.test_recall.compute():.3f}, "
+            f"Test Sensitivity: {self.test_sensitivity.compute():.3f}, "
             f"Test Specificity: {self.test_specificity.compute():.3f}, "
+            f"Test F1: {self.test_f1.compute():.3f}, "
         )
 
     def setup(self, stage: str) -> None:
