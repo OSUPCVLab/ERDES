f30b26448f66a16b829ae4a94c1080ccfc9e4d2d
yaseminozkut <ozkutyasemin@gmail.com>
2025-07-15 17:29:53 -0400

    Update running bash script

--- run_all_normal_vs_rd_exp.sh
+++ run_all_normal_vs_rd_exp.sh
@@ -3,12 +3,12 @@ SESSION="normal_vs_rd_experiment"
 
 tmux new-session -d -s $SESSION
 tmux send-keys -t $SESSION "conda activate erdes" C-m
-tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.devices=3 experiment=normal_vs_rd/efficientnet |& tee tmux_outputs/normal_vs_rd/efficientnet_training.log" C-m
-tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.devices=3 experiment=normal_vs_rd/resnet3d |& tee tmux_outputs/normal_vs_rd/resnet3d_training.log" C-m
-tmux send-keys -t $SESSION "python3 src/train.py trainer=gpu experiment=normal_vs_rd/swinunetr |& tee tmux_outputs/normal_vs_rd/swinunetr_training.log" C-m
-tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.devices=3 experiment=normal_vs_rd/unet3d |& tee tmux_outputs/normal_vs_rd/unet3d_training.log" C-m
-tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.devices=3 experiment=normal_vs_rd/unetplusplus |& tee tmux_outputs/normal_vs_rd/unetplusplus_training.log" C-m
-tmux send-keys -t $SESSION "python3 src/train.py trainer=gpu experiment=normal_vs_rd/unetr |& tee tmux_outputs/normal_vs_rd/unetr_training.log" C-m
-tmux send-keys -t $SESSION "python3 src/train.py trainer=gpu experiment=normal_vs_rd/vit |& tee tmux_outputs/normal_vs_rd/vit_training.log" C-m
-tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.devices=3 experiment=normal_vs_rd/vnet |& tee tmux_outputs/normal_vs_rd/vnet_training.log" C-m
+tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.devices=3 experiment=normal_vs_rd/resnet3d |& tee tmux_outputs/normal_vs_rd/2/resnet3d_training.log" C-m
+#tmux send-keys -t $SESSION "python3 src/train.py trainer=gpu experiment=normal_vs_rd/swinunetr |& tee tmux_outputs/normal_vs_rd/2/swinunetr_training.log" C-m
+tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.devices=3 experiment=normal_vs_rd/unet3d |& tee tmux_outputs/normal_vs_rd/2/unet3d_training.log" C-m
+tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.devices=3 experiment=normal_vs_rd/unetplusplus |& tee tmux_outputs/normal_vs_rd/2/unetplusplus_training.log" C-m
+#tmux send-keys -t $SESSION "python3 src/train.py trainer=gpu experiment=normal_vs_rd/unetr |& tee tmux_outputs/normal_vs_rd/unetr_training.log" C-m
+#tmux send-keys -t $SESSION "python3 src/train.py trainer=gpu experiment=normal_vs_rd/vit |& tee tmux_outputs/normal_vs_rd/vit_training.log" C-m
+tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.devices=3 experiment=normal_vs_rd/vnet |& tee tmux_outputs/normal_vs_rd/2/vnet_training.log" C-m
+tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.devices=3 experiment=normal_vs_rd/efficientnet |& tee tmux_outputs/normal_vs_rd/2/efficientnet_b0_training.log" C-m
 tmux attach -t $SESSION
