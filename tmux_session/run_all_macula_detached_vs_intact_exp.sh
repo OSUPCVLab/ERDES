156a10f73fd3afd27ee70d51ab949c3befe7c8d3
Pouyan <bnpouyan@gmail.com>
2025-07-24 12:42:03 -0400

    added correct tmux bash script

--- tmux_session/run_all_macula_detached_vs_intact_exp.sh
+++ tmux_session/run_all_macula_detached_vs_intact_exp.sh
@@ -3,12 +3,12 @@ SESSION="macula_experiment"
 
 tmux new-session -d -s $SESSION
 tmux send-keys -t $SESSION "conda activate erdes" C-m
-tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.devices=3 experiment=macula_detached_vs_intact/efficientnet |& tee tmux_outputs/macula_detached_vs_intact/2/efficientnet_b0_training.log" C-m
-tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.devices=3 experiment=macula_detached_vs_intact/resnet3d |& tee tmux_outputs/macula_detached_vs_intact/2/resnet3d_training.log" C-m
-#tmux send-keys -t $SESSION "python3 src/train.py trainer=gpu experiment=macula_detached_vs_intact/swinunetr |& tee tmux_outputs/macula_detached_vs_intact/2/swinunetr_training.log" C-m
-tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.devices=3 experiment=macula_detached_vs_intact/unet3d |& tee tmux_outputs/macula_detached_vs_intact/2/unet3d_training.log" C-m
-tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.devices=3 experiment=macula_detached_vs_intact/unetplusplus |& tee tmux_outputs/macula_detached_vs_intact/2/unetplusplus_training.log" C-m
-#tmux send-keys -t $SESSION "python3 src/train.py trainer=gpu experiment=macula_detached_vs_intact/unetr |& tee tmux_outputs/macula_detached_vs_intact/2/unetr_training.log" C-m
-#tmux send-keys -t $SESSION "python3 src/train.py trainer=gpu experiment=macula_detached_vs_intact/vit |& tee tmux_outputs/macula_detached_vs_intact/2/vit_training.log" C-m
-tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.devices=3 experiment=macula_detached_vs_intact/vnet |& tee tmux_outputs/macula_detached_vs_intact/2/vnet_training.log" C-m
+tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.devices=3 experiment=macula_detached_vs_intact/resnet3d |& tee tmux_outputs/macula_detached_vs_intact/resnet3d_training.log" C-m
+tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.devices=3 experiment=macula_detached_vs_intact/swinunetr |& tee tmux_outputs/macula_detached_vs_intact/swinunetr_training.log" C-m
+tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.devices=3 experiment=macula_detached_vs_intact/unet3d |& tee tmux_outputs/macula_detached_vs_intact/unet3d_training.log" C-m
+tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.devices=3 experiment=macula_detached_vs_intact/unetplusplus |& tee tmux_outputs/macula_detached_vs_intact/unetplusplus_training.log" C-m
+tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.strategy=ddp_find_unused_parameters_true trainer.devices=3 experiment=macula_detached_vs_intact/unetr |& tee tmux_outputs/macula_detached_vs_intact/unetr_training.log" C-m
+tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.strategy=ddp_find_unused_parameters_true trainer.devices=3 experiment=macula_detached_vs_intact/vit |& tee tmux_outputs/macula_detached_vs_intact/vit_training.log" C-m
+tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.devices=3 experiment=macula_detached_vs_intact/vnet |& tee tmux_outputs/macula_detached_vs_intact/vnet_training.log" C-m
+tmux send-keys -t $SESSION "python3 src/train.py trainer=ddp trainer.devices=3 experiment=macula_detached_vs_intact/senet |& tee tmux_outputs/macula_detached_vs_intact/senet_training.log" C-m
 tmux attach -t $SESSION
